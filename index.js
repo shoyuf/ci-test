c12cf7b4767bc6fefb1f2d1df7179d5c2c42c9a0
Shoyuf <shoyuf@shoyuf.top>
2023-04-28 14:43:38 +0800

    format output

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.log('start')
-const {context} = require('@actions/github')
-console.log(context)
+console.log("start");
+const { context } = require("@actions/github");
+console.log(JSON.stringify(context, null, 2));
